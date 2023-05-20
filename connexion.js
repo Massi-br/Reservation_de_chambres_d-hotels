86cae0078c6debe7fd87b8064148779d867059eb
MassiBr <massikaneki09@gmail.com>
2023-05-20 16:15:51 +0200

    page d'authentification

--- connexion.js
+++ connexion.js
@@ -1,6 +1,11 @@
+const urlParams = new URLSearchParams(window.location.search);
+const mode = urlParams.get("mode");
+
 const sign_up_link = document.querySelector("#sign-up-link");
 const sign_in_link = document.querySelector("#sign-in-link");
 const container = document.querySelector(".container");
+const sign_in_form = document.querySelector(".sign-in-form");
+const sign_up_form = document.querySelector(".sign-up-form");
 
 sign_up_link.addEventListener("click", () => {
   container.classList.add("sign-up-mode");
@@ -9,3 +14,9 @@ sign_up_link.addEventListener("click", () => {
 sign_in_link.addEventListener("click", () => {
   container.classList.remove("sign-up-mode");
 });
+
+if (mode === "inscription") {
+  container.classList.add("sign-up-mode");
+} else {
+  container.classList.remove("sign-up-mode");
+}
